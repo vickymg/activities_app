262a18c779024d98f29326d780b01a2dfdb5b2d6
Vicky Gray <vickygray26@hotmail.com>
2016-06-20 17:28:20 +0100

    Admin can add a new member - members controller created - new members view created - new members are displayed on the admin page

---
+++ app/controllers/members_controller.rb
@@ -0,0 +1,18 @@
+class MembersController < ApplicationController
+  def new
+    @member = Member.new
+  end
+
+  def create
+    @member = Member.new(member_params)
+    if @member.save
+      redirect_to '/admin'
+    else
+      render 'new'
+    end
+  end
+
+  def member_params
+    params.require(:member).permit(:name)
+  end
+end
--- spec/features/admin_feature_spec.rb
+++ spec/features/admin_feature_spec.rb
@@ -20,4 +20,16 @@ feature 'add members' do
       expect(page).not_to have_content('No members have been added yet.')
     end
   end
+
+  context 'creating members' do
+    scenario 'prompts admin to fill in name, then displays the new member' do
+      visit '/admin'
+      click_link 'Add a new member'
+      expect(current_path).to eq '/members/new'
+      fill_in 'Name', with: 'James Smith'
+      click_button 'Create Member'
+      expect(page).to have_content 'James Smith'
+      expect(current_path).to eq '/admin'
+    end
+  end
 end
