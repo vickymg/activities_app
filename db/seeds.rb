3035cc11ecb43fbd9112086d25e26705684bf96d
Vicky Gray <vickygray26@hotmail.com>
2016-06-23 00:45:10 +0100

    Added seed data for member activities

--- db/seeds.rb
+++ db/seeds.rb
@@ -18,3 +18,14 @@ members = [
 members.each do |name|
   Member.create( name: name )
 end
+
+
+Member.find(1).activities.create(description: 'Ran 26 miles', name: 'Running')
+Member.find(1).activities.create(description: 'Cycled 50 miles to Brighton', name: 'Cycling')
+
+Member.find(2).activities.create(description: 'Ran 3 miles to help with gardening in Islington', name: 'Running')
+Member.find(2).activities.create(description: 'Ran 5 miles to meet Jean', name: 'Running')
+
+Member.find(3).activities.create(description: 'Ran 22 miles in Man v Horse', name: 'Running')
+Member.find(3).activities.create(description: 'Kayaked down the Thames', name: 'Kayaking')
+Member.find(3).activities.create(description: 'Swam the Channel', name: 'Swimming')
